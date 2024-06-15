4788765cdb2eb35217331c4f9e23cc3c6e82fc70
fidansuleymanva <Fidan@LAPTOP-L0IN3B1R>
2024-06-15 17:09:04 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Task2: String name dəyişəni təyin edin əgər bu dəyər sizin adınıza bərabərdirsə "Yes" əks halda "No" çap etsin
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Fidan";
+            Console.WriteLine("Please enter a name:");
+            string inputName = Console.ReadLine();
+
+            if (inputName == name)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
+
